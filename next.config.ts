7353dd877e9a12a267b9edc9dbbedfe66af0c0af
adriansyah-kadir <adriansyahkadir293@gmail.com>
2025-01-13 18:13:38 +0800

    fix: deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/face-attendance-frontend",
+  basePath: "https://adriansyah-kadir.github.io/face-attendance-frontend",
   output: "export",
   trailingSlash: true,
 };
