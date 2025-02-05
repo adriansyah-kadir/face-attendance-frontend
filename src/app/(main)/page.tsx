7c62affede5a9d7f4706b458a81d560bd8e26a36
adriansyah-kadir <adriansyahkadir293@gmail.com>
2025-02-05 18:11:09 +0800

    fix redirect

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
-import { useEffect } from "react"
+import { SessionContext } from "@/lib/providers/session"
+import { useContext, useEffect } from "react"
 
 export default function Home() {
+  const session = useContext(SessionContext)
 
   useEffect(() => {
-    location.replace(process.env.NEXT_PUBLIC_BASE_PATH + "/live")
-  }, [])
+    if (session || session === null) {
+      location.replace(process.env.NEXT_PUBLIC_BASE_PATH + "/live")
+    }
+  }, [session])
 
   return (
     <div>
